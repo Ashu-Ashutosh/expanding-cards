ff63ac6b9e5c9b5792b86d6aa04259580a8f2f31
ASHUTOSH KUMAR <137138520+Ashu-Ashutosh@users.noreply.github.com>
2024-08-20 20:59:48 +0530

    file added

---
+++ script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        // Remove active class from all panels
+        panels.forEach(panel => {
+            panel.classList.remove('active')
+        })
+
+        // Add active class to clicked panel
+        panel.classList.add('active')
+    })
+
+})    
